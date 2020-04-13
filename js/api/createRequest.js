baf36072905c79e1d5e44a4746e44c25badb67d4
Yulia Klyueva <liya-n@list.ru>
2020-04-13 22:35:16 +0300

    js/api/createReques add

--- js/api/createRequest.js
+++ js/api/createRequest.js
@@ -3,5 +3,41 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    const url = options.url;
+    const formData = new FormData;
+    
+    if (options.method === 'GET') {
+        url = url + '?';
+        for(let key in options.data) {
+            url = url + `${key}=${options.data[key]}&`
+        }
+          
+    } else {
+        for(let key in options.data) {
+            formData.append(key, option.data[key]);
+        }
+    }
 
+    try {
+        if (options.method === 'GET') {
+            xhr.open( 'GET', url);
+            xhr.send();
+        } else {
+            xhr.open( options.method, url);
+            xhr.send(formData);
+        }
+      }
+      catch (e) {
+        // перехват сетевой ошибки
+        options.callback(e);
+    }
+
+    xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState === 4 && this.status == 200) {
+            options.callback(null, xhr.response);
+        }
+    });
+    console.log(xhr);
+    return xhr;
 };
